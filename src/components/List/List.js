bda9e2cc27240f188bfaa2757a695a40c8757ed9
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-03-23 08:51:53 +0100

    added material-ui & folder structure

---
+++ src/components/List/List.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Paper} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles'
+import Title from './Title'
+const useStyle = makeStyles({
+    list: {
+        width: '25%',
+        backgroundColor: '#f5f5f5',
+        margin: '10px'
+    },
+})
+
+
+
+const List = () => {
+    const classes = useStyle();
+    return (
+        <Paper elevation={3} className={classes.list}>
+           ffds
+           <Title/>
+        </Paper>
+            
+      
+    )
+}
+
+export default List
